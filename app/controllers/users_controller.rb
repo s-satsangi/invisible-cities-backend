4217532e23ab534d4963801fe302b483e1cb63b0
s-satsangi <s.satsangi@gmail.com>
2021-01-14 17:22:09 -0500

    first go at users create func and user_params in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,19 @@ class UsersController < ApplicationController
     # update? edit?
     # params
     def create
+        byebug
+        @user=User.new(user_params)
+        if !@user.save
+            return render json: {status: "error", message: "some kinda wrong just happend"}
+        end
+        # do user created things. make a token & throw at browser if you do auth
+        render json: {user: @user}, status: :created
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
     end
 
 end
