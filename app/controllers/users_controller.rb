4d18316accecb4e9327ccc8a74a1cfc66a321234
s-satsangi <s.satsangi@gmail.com>
2021-01-14 16:56:38 -0500

    started on users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+    # I think we can get away with all functions being available to non-authenticated users
+    # this controller needs functions to 
+    # create a user
+    # login them in
+    # make them inactive/delete?
+    # update? edit?
+    # params
+    def create
+    end
+
+end
