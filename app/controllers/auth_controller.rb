faa0fd564e71c6c94d187a8fef8b8fd287006891
s-satsangi <s.satsangi@gmail.com>
2021-01-21 00:50:48 -0500

    auth#create looks up user by name

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,16 @@
 class AuthController < ApplicationController
   def create
     # byebug
+    if User.where(username: auth_params[:username])
+      @user = User.where(username: auth_params[:username])
+      return render json: {user: @user}
+    end
+    render json: { message: "user not found.  create an account and start chatting!" }
+  end
+
+  private
+
+  def auth_params
+    params.require(:user).permit(:username)
   end
 end
